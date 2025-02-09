5fbad6e267b4310782935e1cbc8ac6d38e232f64
Brandon Jones <brandonthomasjones@outlook.com>
2025-02-09 20:22:08 +0000

    feat: add vga font map and hello world

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,12 +1,12 @@
 const multiboot = @import("multiboot.zig");
-
+const tty = @import("tty.zig");
 export fn kernel_main(magic: u32, multibootheader: *multiboot.MultibootInfo) void {
     if (magic != 0x2BADB002) {
         return;
     }
 
     const screen: [*]u8 = @ptrFromInt(multibootheader.framebuffer_addr);
-    _ = screen; // silence unused error for now.
+    tty.write_hello_world(screen, multibootheader);
 
     while (true) {}
     return;
