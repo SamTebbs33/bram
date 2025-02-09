828889fd8ba345bdbf0ba5c4ea427905f3b5dad0
Brandon Jones <brandonthomasjones@outlook.com>
2025-02-09 17:15:09 +0000

    feat: parse multiboot information in kmain
    kmain now takes 2 arguments, `magic` and
    a pointer to the `multibootinfo` struct.

--- src/kernel.zig
+++ src/kernel.zig
@@ -2,14 +2,23 @@ const std = @import("std");
 const builtin = @import("builtin");
 const serial = @import("serial.zig");
 const arch = @import("arch.zig");
+const multiboot = @import("multiboot.zig");
 
 pub fn log(comptime level: std.log.level, comptime scope: @TypeOf(.EnumLiteral), comptime format: []const u8, args: anytype) void {
     serial.log(level, "(" ++ @tagName(scope) ++ "): " ++ format, args);
 }
 
-export fn kernel_main() void {
+export fn kernel_main(magic: u32, multibootheader: *multiboot.MultibootInfo) void {
+    if (magic != 0x2BADB002) {
+        return;
+    }
+
     const arch_ifc = arch.get(builtin.cpu);
     if (!serial.init(&arch_ifc))
         return;
-    return;
+
+    const screen: [*]u8 = @ptrFromInt(multibootheader.framebuffer_addr);
+    _ = screen; // silence unused error for now.
+
+    while (true) {}
 }
